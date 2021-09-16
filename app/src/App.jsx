c385dcb2255113d1e1e35c813099ceacfe75aa58
Eric Latham <ericoliverlatham@gmail.com>
2021-09-16 09:40:11 -0500

    add instructions to refresh page to restart POC simulation

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <h1>Smart Home Dashboard Simulator</h1>
+      <h4>(refresh page to restart simulation)</h4>
       <AppClock interval={10000} />
       <EventStream />
     </div>
