8631d532b50a2edc60ea62c94398720458949ce5
Laura Thompson <laurthompson0@gmail.com>
2021-10-03 15:15:28 -0500

    Create incomplete framework for data generation

--- app/generate_events.py
+++ app/generate_events.py
@@ -1,119 +1,130 @@
 """
 Generate state of home for two months
 """
-
-class PredeterminedState():
-    def __init__():
-        # See excel
-
-# Send events - smallest increment of change = 30 sec door event
-
-# Questions
-# When are lightbulbs and bath exhaust fan on?
-# Is there water usage other than showers/washers?
-# When are windows open?
-
-# State rules
-# Outdoor temp: From weather API
-# Doors: 30 sec/event, 16 events/day M-F, 32 events/day S-S
-# Fridge: always running
-# Microwave: 20 min/day M-F, 30 min/day S-S
-# Stove: 15 min/day M-F, 30 min/day S-S
-# Oven: 45 min/day M-F, 60 min/day S-S
-# LR TV: 4 hrs/day M-F, 8 hrs/day S-S
-# Bedroom TV: M-F 2 hrs/day, 4 hrs/day S-S
-# Shower: 2/day M-F, 3/day S-S, 25 gal (65% hot, 35 % cold)
-# Bath: 2/day M-F, 3/day S-S, 30 gal (65% hot, 35 % cold)
-# Dishwasher: 45 min/load, 4 load/week
-# Clothes washer: 30 min/load, 4 loads/week
-# Clothes dryer: 30 min/load, 4 loads/week
-
-# Approximate Family Schedule M-F
-# 5am - adults wake up
-# 6am - kids wake up
-# 7:30am - adults/kids leave
-# 4pm - kids arrive home
-# 5:30pm - adults arrive home
-# 8:30pm - kids sleep
-# 10:30pm - adults sleep
-
-# Events M-F
-# 7:15-7:45a - 4 door events
-# 3:45-4:15a - 2 door events
-# 5:15-5:45a - 2 door events
-# 6:00-8:00a - 8 door events
-
-# 5-6a - 5 min microwave event
-# 6-7:15a - 5 min microwave event
-# 4:15-4:45p - 5 min microwave event
-# 4:45-5:15p - 5 min microwave event
-
-# 5:45-7p - 15 min stove event
-# 5:45-7p - 45 min oven event
-
-# 4:15-10:00 - 4 hour LR TV event
-# 8:00 - 10:30 2 hr BR TV event
-
-# 5:30-6:15a - 15 min shower event
-# 6:15-7:00a - 15 min shower event
-# 6-7p - 15 min bath event
-# 7-8p - 15 min bath event
-
-#Events S-S
-# 7a-10p - 30 sec door event x32 (non overlapping)
-# 7a-10p - 5 min microwave event x6 (non overlapping)
-# 5p-7p - 30 min stove event
-# 4p-7p - 60 min oven event
-
-# 7a-10p - 8 hr LR TV event
-# 6a-10a - 2 hr BR TV event
-# 7p-10p - 2 hr BR TV event
-
-# 6-7a - 15 min shower event
-# 7-8a - 15 min shower event
-# 11-12p - 15 min shower event
-# 12-1p - 15 min bath event
-# 6-7p - 15 min bath event
-# 7-8p - 15 min bath event
-
-# Any day
-# 7-10p (START) - 45 min dishwasher event (4 times/week)
-# 7-10p - 60 min wash/dry event (2 times/ M-F)
-# 8a-10p - 60 min wash/dry event S-S, x2 (non overlapping)
+import random
+import psycopg2
+import datetime
+from meteostat import Hourly, Point
 
 
-# Calculation Rules
+TIME_MAP = {
+    #Assumes 0 = Midnight on Monday
+    "minute": 60,
+    "hour": 3600,
+    "day": 86400,
+    "week": 604800,
+    "month": 2592000, #Assumes 30 days
+    "Tuesday": 86400,
+    "Wednesday": 172800,
+    "Thursday": 259200,
+    "Friday": 345600,
+    "Saturday": 432000,
+    "Sunday": 518400,
+}
 
-# Interior temp - +/- 2 deg/hr per 10 deg outside
-# +/- 2 deg/hr per 10 deg dif outside per 5 min door is open
-# +/- 1 deg/hr/10 deg dif outside per 5 min window open
+class StateGenerator:
+    def __init__(self, db, start, weather_location):
+        """
+        Define initial state
+        """
+        """
+        self.boolean_state_types = ["door, window", "light", "bedRoomTv",
+            "livingRoomTv", "stove", "oven", "microwave",
+            "refrigerator", "dishWasher", "shower", "bath",
+            "bathExhaustFan", "clothesWasher", "clothesDryer"]
+        self.boolean_state_keys = "bedRoom1OverheadLight", "bedRoom1Lamp1", "bedRoom1Lamp2", "bedRoom1Window1",
+            "bedRoom1Window2", "bedRoom1Tv", "bedRoom2OverheadLight", "bedRoom2Lamp1", "bedRoom2Lamp2", "bedRoom2Window1",
+            "bedRoom2Window2", "bedRoom2OverheadLight", "bedRoom2Lamp1", "bedRoom2Lamp2", "bedRoom2Window1", "bedRoom2Window2", 
+            "bathRoom1OverheadLight", "bathRoom1ExhaustFan", "bathRoom1Window", "bathRoom1Faucet", "bathRoom2OverheadLight",
+            "bathRoom2ExhaustFan", "bathRoom2Window", "bathRoom2Faucet", "livingRoomOverheadLight", "livingRoomLamp1",
+            "livingRoomLamp2", "livingRoomTv", "livingRoomWindow1", "livingRoomWindow2", "livingRoomWindow3", "kitchenOverheadLight",
+            "kitchenStove", "kitchenOven", "kitchenMicrowave", "kitchenRefrigerator", "kitchenDishWasher", "kitchenWindow1",
+            "kitchenWindow2", "garageHouseDoor", "garageCarDoor1", "garageCarDoor2", "frontDoor", "backDoor", "clothesWasher", "clothesDryer"]
+        self.integer_state_types = ["temp"]
+        self.integer_state_keys = ["outdoorTemp", "thermostatTemp"]
+        """
 
-# HVAC +/- 1 degree for every minute of operation, maintains set temp 
-# within 2 degrees, HVAC - 3500 W
+        self.db = db
+        self.weather_location = weather_location
+        self.start = start
+        #Assert start time is midnight, on monday, and at least 60 days prior:
+        if not (start.weekday() == 0 and start.hour == 0 and start.minute == 0 and start.second == 0 and ((start + datetime.timedelta(61)) <= datetime.datetime.today())):
+            start_date = datetime.date.today() - datetime.timedelta(61)
+            week_day = start_date.weekday()
+            start_date = start_date - datetime.timedelta(week_day)
+            self.start = datetime.datetime(start_date.year, start_date.month, start_date.day)
 
-# Light bulbs - 60 W, 15 lights, running when?
-# Bath fan, 30W, 2 fans, running when?
-# Fridge - 150 W, running always
-# Microwave - 1100 W, 20 min/day M-F, 30 min/day S-S
-# Stove - 3500 W, 15 min/day M-F, 30 min/day S-S
-# Oven - 4000 W, 45 min/day M-F, 60 min/day S-S
-# LR TV - 636 W, 4 hrs/day M-F, 8 hrs/day S-S
-# B TV - 100 W, 2 hrs/day M-F, 4 hrs/day S-S
+    def generateIntialState():
+        # Assume t0 is Midnight on Monday
+        pass
 
-# Shower: 2/day M-F, 3/day S-S, 25 gal (65% hot, 35 % cold)
-# Bath: 2/day M-F, 3/day S-S, 30 gal (65% hot, 35 % cold)
+    def generateWeatherData(self):
+        """ Generate hourly weather data"""
+        # TODO Convert to F, insert into DB, check that correct num are inserted
+        start = self.start
+        end = self.start + datetime.timedelta(60)
+        loc = self.weather_location
+        try:
+            weather_data = Hourly(loc, start, end)
+            weather_data = weather_data.fetch()
+            print(weather_data.temp)
+        except Exception as e:
+            e.PrintStackTrace()
 
-# Dishwasher: 1800 W, 45 mins/load, 4 load/week, 6 gal hot water/load
-# Clothes Washer: 500 W, 20 gal/load (85% hot water, 15% cold), 30 min/load, 4 load/week
-# Clothes Dryer: 3000 W, 30 min/load, 4 load/week
 
-# Electricity = $0.12 per kWh
-# Hot Water Heater - 4500 W, 4 mins to heat 1 gal/water
-# Water = $2.52 per 100 cubic feet of water
 
-# Variable Calculations:
-# Indoor Temperature/HVAC
+    def generateFrontBackDoors():
+        # Assume t0 and t1 are always initial and final time
 
-# Static Calculations:
-# (If desired we could make these slightly less static = e.g. hourly)
-# Light bulbs, fans, microwave, stove, oven, TVs, Showers/Baths, Dishwasher, Clothes
+        #Iterate over each day
+        for t_day in range(0, 60*TIME_MAP["day"], TIME_MAP["day"]):
+            # S-S
+            if ((t_curr % TIME_MAP["Saturday"]) == 0 ) or ((t_curr % TIME_MAP["Sunday"]) == 0):
+                #7a-10p - 30 sec door event x32 (non overlapping)
+                t_start = t_day + 7 * TIME_MAP["hour"]
+                t_end = t_day + 22 * TIME_MAP["hour"]
+                for i in range(32):
+                    # HOW TO AVOID OVERLAP?
+
+                    #Randomly pick which door to open
+                    pick_door = random.random()
+                    which_door = "frontDoor"
+                    if pick_door < 0.5:
+                        which_door = "backDoor"
+                    
+                    t_open = random.randInt(t_start, t_end)
+                    t_close = t_open + 30
+
+    def insert(table, time, state_type, state_key, new_value, message):
+        if table in ["boolean_event", "integer_event"]:
+            try:
+                self.db.execute(f"INSERT INTO pre_generated_events.{table} VALUES (%s, %s, %s, %s, %s)", 
+                time, state_type, state_key, new_value, message)
+            except Exception as e:
+                e.PrintStackTrace()
+        else:
+            print("Could not insert into table" + table)
+
+
+def main():
+
+    #conn = psycopg2.connect(dbname="smart_home_simulation", user="username", password="password", host="localhost", port="5432")
+    #cur = conn.cursor()
+
+    start_date = datetime.date.today() - datetime.timedelta(61)
+    week_day = start_date.weekday()
+    start_date = start_date - datetime.timedelta(week_day) #make start day a monday
+    start_datetime = datetime.datetime(start_date.year, start_date.month, start_date.day)
+
+    weather_location = Point(33.5186, -86.8104) #Birmingham
+
+    stateGenerator = StateGenerator(None, start_datetime, weather_location)
+
+    stateGenerator.generateWeatherData()
+    #stateGenerator.generateInitialState()
+    #stateGenerator.generateFrontBackDoors()
+    
+
+
+if __name__ == "__main__":
+    main()
