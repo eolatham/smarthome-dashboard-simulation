96333bda84b4fce43f55fa112d7af47d75920c02
Laura Thompson <laurthompson0@gmail.com>
2021-10-04 09:55:20 -0500

    Generate Initial states

--- app/generate_events.py
+++ app/generate_events.py
@@ -54,20 +54,83 @@ class StateGenerator:
             start_date = start_date - datetime.timedelta(week_day)
             self.start = datetime.datetime(start_date.year, start_date.month, start_date.day)
 
-    def generateIntialState():
-        # Assume t0 is Midnight on Monday
-        pass
+    def generateIntialState(self):
+        """ Generates initial state, assuming t = 0 is a Monday at Midnight"""
+        initial_states =[[0, "light", "bedRoom1OverheadLight", False, "Bedroom 1 Overhead Light is OFF"],
+                        [0, "light", "bedRoom1Lamp1", False, "Bedroom 1 Lamp 1 is OFF"],
+                        [0, "light", "bedRoom1Lamp2", False, "Bedroom 1 Lamp 2 is OFF"],
+                        [0, "window", "bedRoom1Window1", False, "Bedroom 1 Window 1 is CLOSED"],
+                        [0, "window", "bedRoom1Window2", False, "Bedroom 1 Window 2 is CLOSED"],
+                        [0, "bedRoomTv", "bedRoom1Tv", False, "Bedroom 1 TV is OFF"],
+
+                        [0, "light", "bedRoom2OverheadLight", False, "Bedroom 2 Overhead Light is OFF"],
+                        [0, "light", "bedRoom2Lamp1", False, "Bedroom 2 Lamp 1 is OFF"],
+                        [0, "light", "bedRoom2Lamp2", False, "Bedroom 2 Lamp 2 is OFF"],
+                        [0, "window", "bedRoom2Window1", False, "Bedroom 2 Window 1 is CLOSED"],
+                        [0, "window", "bedRoom2Window2", False, "Bedroom 2 Window 2 is CLOSED"],
+
+                        [0, "light", "bedRoom3OverheadLight", False, "Bedroom 3 Overhead Light is OFF"],
+                        [0, "light", "bedRoom3Lamp1", False, "Bedroom 3 Lamp 1 is OFF"],
+                        [0, "light", "bedRoom3Lamp2", False, "Bedroom 3 Lamp 2 is OFF"],
+                        [0, "window", "bedRoom3Window1", False, "Bedroom 3 Window 1 is CLOSED"],
+                        [0, "window", "bedRoom3Window2", False, "Bedroom 3 Window 2 is CLOSED"],
+
+                        [0, "light", "bathRoom1OverheadLight", False, "Bathroom 1 Overhead Light is OFF"],
+                        [0, "bathExhaustFan", "bathRoom1ExhaustFan", False, "Bathroom 1 Exhaust Fan is OFF"],
+                        [0, "window", "bathRoom1Window", False, "Bathroom 1 Window is CLOSED"],
+                        [0, "shower", "bathRoom1Shower", False, "Bathroom 1 Shower is OFF"], #NOTE THIS IS DIFFERENT THAN BATHROOM FAUCET
+                        [0, "bath", "bathRoom1Bath", False, "Bathroom 1 Bath is OFF"],
+
+                        [0, "light", "bathRoom2OverheadLight", False, "Bathroom 2 Overhead Light is OFF"],
+                        [0, "bathExhaustFan", "bathRoom2ExhaustFan", False, "Bathroom 2 Exhaust Fan is OFF"],
+                        [0, "window", "bathRoom2Window", False, "Bathroom 2 Window is CLOSED"],
+                        [0, "shower", "bathRoom2Shower", False, "Bathroom 2 Shower is OFF"], #NOTE THIS IS DIFFERENT THAN BATHROOM FAUCET
+                        [0, "bath", "bathRoom2Bath", False, "Bathroom 2 Bath is OFF"],
+
+                        [0, "light", "livingRoomOverheadLight", False, "Living Room Overhead Light is OFF"],
+                        [0, "light", "livingRoomLamp1", False, "Living Room Lamp 1 is OFF"],
+                        [0, "light", "livingRoomLamp2", False, "Living Room Lamp 2 is OFF"],
+                        [0, "window", "livingRoomWindow1", False, "Living Room Window 1 is CLOSED"],
+                        [0, "window", "livingRoomWindow2", False, "Living Room Window 2 is CLOSED"],
+                        [0, "window", "livingRoomWindow3", False, "Living Room Window 2 is CLOSED"],
+                        [0, "livingRoomTv", "livingRoomTv", False, "Living Room TV is OFF"],
+
+                        [0, "light", "kitchenOverheadLight", False, "Kitchen Overhead Light is OFF"],
+                        [0, "stove", "kitchenStove", False, "Kitchen Stove is OFF"],
+                        [0, "oven", "kitchenOven", False, "Kitchen Oven is OFF"],
+                        [0, "microwave", "kitchenMicrowave", False, "Kitchen Microwave is OFF"],
+                        [0, "refrigerator", "kitchenRefrigerator", True, "Kitchen Refrigerator is ON"],
+                        [0, "dishWasher", "kitchenDishWasher", False, "Kitchen Dishwasher is OFF"],
+                        [0, "window", "kitchenWindow1", False, "Kitchen Window 1 is CLOSED"],
+                        [0, "window", "kitchenWindow2", False, "Kitchen Window 2 is CLOSED"],
+
+                        [0, "door", "garageCarDoor1", False, "Garage Car Door 1 is CLOSED"],
+                        [0, "door", "garageCarDoor2", False, "Garage Car Door 2 is CLOSED"],
+                        [0, "door", "garageHouseDoor", False, "Garage House Door is CLOSED"],
+                        [0, "door", "frontDoor", False, "Front Door is CLOSED"],
+                        [0, "door", "backDoor", False, "Back Door is CLOSED"]]
+        
+        for initial_state in initial_states:
+            self.insert(initial_state[0], initial_state[1], initial_state[2], initial_state[3], initial_state[4])
+
+        #Integer initial states
+        self.insert(0, "temp", "thermostatTemp", 70, "Thermostat is set to 70")
+        
+        self.insert()
 
     def generateWeatherData(self):
         """ Generate hourly weather data"""
-        # TODO Convert to F, insert into DB, check that correct num are inserted
         start = self.start
         end = self.start + datetime.timedelta(60)
         loc = self.weather_location
         try:
             weather_data = Hourly(loc, start, end)
             weather_data = weather_data.fetch()
-            print(weather_data.temp)
+            for i in range(len(weather_data)):
+                temp = int(5/9*weather_data[i].temp + 32
+                #self.insert("integer_event", i*TIME_MAP["hour"], "temp", "outdoorTemp", temp, "The current outdoor temperature is "+str(temp)))
+                weather_vals = ["integer_event", i*TIME_MAP["hour"], "temp", "outdoorTemp", temp, "The current outdoor temperature is "+str(temp)]
+                print(weather_vals)
         except Exception as e:
             e.PrintStackTrace()
 
@@ -98,8 +161,10 @@ class StateGenerator:
     def insert(table, time, state_type, state_key, new_value, message):
         if table in ["boolean_event", "integer_event"]:
             try:
-                self.db.execute(f"INSERT INTO pre_generated_events.{table} VALUES (%s, %s, %s, %s, %s)", 
-                time, state_type, state_key, new_value, message)
+                self.db.execute((f"INSERT INTO pre_generated_events.{table} "
+                                "VALUES (%s, %s, %s, %s, %s) "
+                                "ON CONFLICT DO UPDATE"), 
+                                time, state_type, state_key, new_value, message)
             except Exception as e:
                 e.PrintStackTrace()
         else:
